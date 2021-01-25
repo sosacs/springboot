6d1ec9938a95119a05cd5574000e1d03eb55ae0b
Cristian Sosa <cristian.sosa@tecnotree.com>
2021-01-25 01:22:12 -0300

    base proyecto

---
+++ spring-boot-csosa/src/main/java/com/empresa/springboot/app/SpringBootCsosaApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCsosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCsosaApplication.class, args);
+	}
+
+}
---
+++ spring-boot-csosa/src/test/java/com/empresa/springboot/app/SpringBootCsosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCsosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
